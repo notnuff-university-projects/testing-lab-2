55406a86d8bf06155a777d3e4e978bbbd8d7a646
notnuff <113949291+notnuff@users.noreply.github.com>
2024-12-07 01:23:39 +0200

    made parser implementation

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,5 +1,23 @@
 #include "parser.h"
 
-std::vector<char> Parse(const std::string& input) {
+#include <stdexcept>
 
+namespace parser {
+    std::vector<char> Parse(const std::string& input) {
+        std::vector<char> result;
+
+        for(int i = 0; i < input.size(); i++) {
+            if( input[i] == ' ' ) continue;
+            if( input[i] == '\n' && i==input.size() - 1 ) continue;
+
+
+            result.push_back(input[i]);
+
+            if(auto nextI = i + 1; nextI < input.size() && input[nextI] != ' ' ) {
+                throw std::runtime_error("Wrong format");
+            }
+        }
+
+        return result;
+    }
 }
